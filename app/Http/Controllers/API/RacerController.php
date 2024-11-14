3a21cf6adbfb3c491dfb9512013edead5b200925
Molten-Verb <sanches-9868@yandex.ru>
2024-11-14 23:11:17 +0300

    Переименовал

--- app/Http/Controllers/API/RacerController.php
+++ app/Http/Controllers/API/RacerController.php
@@ -8,13 +8,12 @@ use App\Http\Controllers\Controller;
 use App\Http\Resources\RacerResource;
 use Spatie\QueryBuilder\QueryBuilder;
 
-class RacerAPI extends Controller
+class RacerController extends Controller
 {
     public function index(Request $request)
     {
         $racers = QueryBuilder::for(Racer::class)
             ->allowedFilters('name', 'country', 'price');
-            // например http://127.0.0.1:8000/api?filters[price]=1000000
 
         return RacerResource::collection($racers->jsonPaginate());
     }
--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\API\RacerAPI;
+use App\Http\Controllers\API\RacerController;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,5 +19,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/racers', [RacerAPI::class, 'index']);
-// запрос вводить с префиксом /api, (добавляется автоматически посредником)
+Route::get('/racers', [RacerController::class, 'index']);
+// /api/racers
